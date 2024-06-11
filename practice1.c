54e6b7cc7e81073659a266c1b0ea59270f081495
mahidulislam12 <mahidul2023islam20@gmail.com>
2024-06-11 22:05:36 +0600

    Add files via upload

---
+++ practice1.c
@@ -0,0 +1,57 @@
+/* 1#include<stdio.h>
+int main(){
+    printf("Recently I heard that you\'ve achieved\n95%% marks in your exam.\n");
+    printf("This is brilliant!\n");
+    printf("I wish you\'ll shine in your life!\tGood\nluck with all the barriers(/\\) in your life.");
+    return 0;
+}*/
+/*
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    int sum= a +b;
+    int m=a*b;
+    printf("%d + %d = %d\n", a, b, sum);
+    printf("%d*%d=%d\n",a,b,m);
+    return 0;
+}*/
+
+
+/*#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    if(n%2==0){
+        printf("even");
+    }
+    else{
+        printf("odd");
+    }
+    return 0;
+}
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    if(n>0){
+        printf("positive");
+    }
+    else if (n<0){
+        printf("negative");
+    }
+    else{
+        printf("zero");
+    }
+    {
+        /* code */
+    }
+    
+    
+    return 0;
+}
