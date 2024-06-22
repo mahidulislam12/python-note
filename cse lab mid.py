9da71bd77ca680a2b97b187ba89c83b90580406b
mahidulislam12 <mahidul2023islam20@gmail.com>
2024-06-22 20:14:39 +0600

    Add files via upload

---
+++ cse lab mid.py
@@ -0,0 +1,38 @@
+"""x="“Okay, here we go!”"
+
+print(x[1:5])
+print(x[-4:-2])"""
+"""
+i=20
+while i<=100:
+    if (40<=i<=50) or (i==65):
+        i=i+1
+        continue
+    print(i)
+    i=i+1
+"""
+"""
+i = 1
+while (i <= 100):
+    if i > 1 and i < 70:
+        print(i)
+    i += 1"""
+'''
+i = 11
+while i <= 100:
+    if i == 50 or i == 65 or (i >= 90 and i <= 95):
+        i += 1
+        continue
+    print(i)
+    i += 1
+'''
+for i in range(-1,-100,-1):
+    if(i==65 or(70<=i<=75)):
+        continue
+    print(i)
+    
+
+
+ 
+
+
