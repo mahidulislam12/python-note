44873ddb69913081968fe57f35aee31c3b631d81
mahidulislam12 <mahidul2023islam20@gmail.com>
2024-08-11 17:45:14 +0600

    Add files via upload

---
+++ hsorting.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter the value of a:");
+    scanf("%d",&a);
+    int arr[a];
+    printf("enter the arr:");
+    for(int i=0;i<a;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<a-1;i++){
+        for(int j=i+1;j<a;j++){
+            if(arr[i]>arr[j]){
+                int tmp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=tmp;
+            }
+        }
+        
+        }
+    for(int i=0;i<a;i++){
+         printf("%d ",arr[i]);
+    }
+    return 0;
+
+}
