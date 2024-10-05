24b3203a0829ac93efdc4e451d593ab69b3dd083
mahidulislam12 <mahidul2023islam20@gmail.com>
2024-10-05 06:24:10 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#using return for exit
+"""""def frist_even_num(numbers):
+    for num1 in numbers:
+        if num1%2==0:
+            return  num1
+    return None
+a1=frist_even_num([10,45,54,34,40])
+print(a1)"""""
+#lamda function
+'''''result=lambda :2+3
+print(result())'''''
+#lemda function as a parameter
+#result=lambda x,y: x+y
+#print(result(10,50))
+#condition as lambda function
+result=lambda x,y: x if x>y else y
+print(result(10,50))
+
+
